05160360ca0bb1f2c59503fd0fb3c0d9dbb18589
HWWHEN <1145908327@qq.com>
2022-08-30 10:14:54 +0800

    updata

--- dist/apiConfigPre.js
+++ dist/apiConfigPre.js
@@ -1,4 +1,4 @@
-window.baseURL = "https://preapi-marketplace.mechainfinity.com/api/v1";
+window.baseURL = "http://119.8.123.183/api/v1";
 window.checkNetWork = false, //是否开启小狐狸钱包网络检测和切换
 window.metaMaskNetWork = {
   chainId: "0x89", // 网络id，16进制的字符串
@@ -12,4 +12,4 @@ window.metaMaskNetWork = {
     decimals: 18,
   },
 };
-window.usdtToken = '0x42eeb33D1354B2bbE452D62877eE804B572B9961'
+window.usdtToken = '0x1F0eD56B5d2fc04E70a7926DE024955D6769Ffea'
